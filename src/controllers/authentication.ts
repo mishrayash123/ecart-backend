658f4b786da67f1b91a4fc2b961877ed7f7668da
yash kumar mishra <mishrayash3778@gmail.com>
2023-12-28 12:52:42 +0530

    fhej

--- src/controllers/authentication.ts
+++ src/controllers/authentication.ts
@@ -28,7 +28,10 @@ export const login = async (req: express.Request, res: express.Response) => {
 
     await user.save();
 
-    res.cookie('YashKumarMishra-auth', user.authentication.sessionToken, { domain: 'localhost', path: '/' });
+    res.cookie('YashKumarMishra-auth', user.authentication.sessionToken, {
+      expires: new Date (Date.now() + 25892000000),
+      httpOnly:true
+      });
 
     return res.status(200).json(user).end();
   } catch (error) {
